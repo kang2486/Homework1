02ab791689fb060dbd96f39e6e1fa05d471e41c7
kang2486 <41343122@nfu.edu.tw>
2025-10-13 14:02:25 +0800

    Update problem02.cpp

--- homework01/src/problem02.cpp
+++ homework01/src/problem02.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-
 // 遞迴函數來產生所有子集
 void generatePowerset(char* set, int setSize, int index, char* subset, int subsetSize) {
     if (index == setSize) {
@@ -16,27 +15,20 @@ void generatePowerset(char* set, int setSize, int index, char* subset, int subse
         printf("}\n");
         return;
     }
-
     // 選擇 set[index]
     subset[subsetSize] = set[index];
     generatePowerset(set, setSize, index + 1, subset, subsetSize + 1);
-
     // 不選擇 set[index]
     generatePowerset(set, setSize, index + 1, subset, subsetSize);
 }
-
 int main() {
     // 範例集合 S = {a, b, c}
     char set[] = {'a', 'b', 'c'};
     int setSize = sizeof(set) / sizeof(set[0]);
-
     // 最大子集長度 = setSize，準備暫存陣列
     char* subset = (char*)malloc(setSize * sizeof(char));
-
     printf("Powerset of {a, b, c}:\n");
     generatePowerset(set, setSize, 0, subset, 0);
-
     free(subset); // 釋放記憶體
-
     return 0;
 }
