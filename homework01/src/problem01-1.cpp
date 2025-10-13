a0fc8c9ed01f2c323242821f0cad139aed9a697c
kang2486 <41343122@nfu.edu.tw>
2025-10-13 13:36:08 +0800

    Update problem01-1.cpp

--- homework01/src/problem01-1.cpp
+++ homework01/src/problem01-1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+// Ackermann function (recursive)
+int Ackermann(int m, int n) {
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Ackermann(m - 1, 1);
+    else
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+int main() {
+    int m, n;
+    cout << "Enter m and n: ";
+    cin >> m >> n;
+
+    cout << "Ackermann(" << m << ", " << n << ") = " << Ackermann(m, n) << endl;
+    return 0;
+}
