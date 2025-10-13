88877079bfd09eb8da71762a1dfb983dc9a80c38
kang2486 <41343122@nfu.edu.tw>
2025-10-13 13:42:01 +0800

    Update problem02.cpp

--- homework01/src/problem02.cpp
+++ homework01/src/problem02.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+void powerSetRecursive(vector<string>& set, int index, vector<string> current) {
+    if (index == set.size()) {
+        cout << "{ ";
+        for (auto& e : current) cout << e << " ";
+        cout << "}" << endl;
+        return;
+    }
+
+    // 不包含當前元素
+    powerSetRecursive(set, index + 1, current);
+
+    // 包含當前元素
+    current.push_back(set[index]);
+    powerSetRecursive(set, index + 1, current);
+}
+
+int main() {
+    vector<string> S = { "a", "b", "c" };
+    cout << "Power set of {a, b, c}:\n";
+    vector<string> current;
+    powerSetRecursive(S, 0, current);
+    return 0;
+}
